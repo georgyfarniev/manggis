89d0090b0b7c648ad8116fec774a8b5339057f16
Georgy Farniev <gfarniev@icloud.com>
2021-01-31 21:09:41 +0800

    add typescript

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+async function main() {
+  /**
+   * 1. Load schema object
+   * 2. Validate all documents in collection with schema
+   * 3. Populate all required refs and ensure their integrity. Allow option to validate all refs, 
+   *    not only required ones.
+   * 4. Create convenient report on per-collection basis
+   */
+
+
+}
+
+if (require.main === module) {
+  main()
+}
