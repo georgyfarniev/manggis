3a044d0cd3b4421560618143510c7697a9447286
Georgy Farniev <gfarniev@icloud.com>
2021-03-08 18:25:06 +0800

    use context pattern

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ interface IValidationOptions {
   tempModelNamePrefix: string
   verifySchema: boolean
   verifyRefs: boolean
-  onError?: OnErrorHook
+  onError: OnErrorHook
 }
 
 interface IValidationError {
@@ -37,14 +37,29 @@ interface IValidationError {
   message: string
 }
 
+interface IValidationContext {
+  options: IValidationOptions
+  connection: Connection
+  model?: Model<any>
+  doc?: Document
+  err?: mongoose.Error.ValidationError
+}
+
 async function verifyMongooseConnection(connection: Connection, opts: IValidationOptions) {
   assert(opts.verifyRefs || opts.verifySchema, 'at least one validation option is a must');
+
+  const ctx: IValidationContext = {
+    connection,
+    options: opts,
+  }
 
   const models = Object.values(connection.models);
 
   for (const model of models) {
     // Only for refs need to create custom model
-    const validationModel = opts.verifyRefs ? createEnhancedModel(connection, model, opts) : model;
+    const validationModel = opts.verifyRefs ? createEnhancedModel({ ...ctx, model }) : model;
+
+    // ctx.model = validationModel;
 
     const count = await validationModel.countDocuments();
     logger.info(`Checking model: ${model.modelName}, documents count: ${count}`);
@@ -53,51 +68,56 @@ async function verifyMongooseConnection(connection: Connection, opts: IValidatio
     for await (const doc of cursor) {
       logger.info('    Checking doc: ' + doc._id)
       if (opts.verifySchema) {
-        await verifySchemaForDocument(doc, opts);
+        const docCtx = { ...ctx, model: validationModel, doc }
+        await verifySchemaForDocument(docCtx);
       }
     }
 
-    mongoose.deleteModel(validationModel.modelName)
+    if (opts.verifyRefs) {
+      mongoose.deleteModel(validationModel.modelName)
+    }
   }
 
   logger.info('all models has been verified');
   process.exit();
 }
 
+function createEnhancedModel(ctx: IValidationContext) {
+  const schema = ctx.model!.schema.clone();
 
-function createEnhancedModel(connection: Connection, model: Model<any>, opts: IValidationOptions) {
-  const schema = model.schema.clone();
+  const { tempModelNamePrefix, verifyRefs } = ctx.options
 
-  if (opts.verifyRefs) {
-    installRefValidator(connection, schema);
+  if (verifyRefs) {
+    installRefValidator(ctx.connection, schema);
   }
 
-  const name = `${opts.tempModelNamePrefix}${model.modelName}`
-  return mongoose.model(name, schema, model.collection.collectionName);
+  const name = `${tempModelNamePrefix}${ctx.model!.modelName}`
+  return mongoose.model(name, schema, ctx.model!.collection.collectionName);
 }
 
-async function verifySchemaForDocument(doc: Document, opts: IValidationOptions) {
+async function verifySchemaForDocument(ctx: IValidationContext) {
   try {
-    await doc.validate();
+    await ctx.doc!.validate();
   } catch (err) {
-    if (err instanceof mongoose.Error.ValidationError && opts.onError) {
-      await reportValidationErrors(doc, err, opts.onError);
+    const { onError } = ctx.options;
+
+    if (err instanceof mongoose.Error.ValidationError && onError) {
+      await reportValidationErrors({ ...ctx, err });
     } else {
-      // fatal error
+      assert(false, 'Unknown error!');
     }
-
-    // console.log('reason:', err?.errors?.bar?.properties?.message)
-
-    // const errorMessage = err?.message?.replace(MG_TEMP_MODEL_PREFIX, '')
-    // logger.error(`validation error for doc with id: ${doc._id}: ${errorMessage}`)
   }
 }
 
-async function reportValidationErrors(doc: Document, err: mongoose.Error.ValidationError, cb: OnErrorHook) {
-  for (const error of Object.values(err.errors)) {
-    await cb({
-      document: doc._id.toString(),
-      model: doc.modelName,
+async function reportValidationErrors(ctx: IValidationContext) {
+  const { options, doc, err } = ctx;
+  const { onError, tempModelNamePrefix } = options;
+  const model = ctx.model!.modelName.replace(tempModelNamePrefix, '');
+
+  for (const error of Object.values(err!.errors)) {
+    await onError({
+      document: doc!._id.toString(),
+      model,
       path: error.path,
       message: error.message
     })
@@ -112,7 +132,11 @@ function installRefValidator(connection: Connection, schema: Schema) {
     } else if (type.options.ref) {
       type.validate({
           validator: async (value: any) => {
-            const refModel = connection.models[type.options.ref];
+            const { ref } = type.options;
+            const refModel = connection.models[ref];
+
+            assert(refModel, `Referenced model not found: ${ref}`);
+
             const exists = await refModel.exists({ _id: value });
             return exists;
           },
@@ -122,6 +146,8 @@ function installRefValidator(connection: Connection, schema: Schema) {
   }
 }
 
+// -------------------------------------------------------------------------------------------------
+
 const MG_TEMP_MODEL_PREFIX = '__manggis_model__';
 
 async function main() {
@@ -130,7 +156,7 @@ async function main() {
 
   await verifyMongooseConnection(connection, {
     tempModelNamePrefix: MG_TEMP_MODEL_PREFIX,
-    verifyRefs: true,
+    verifyRefs: false,
     verifySchema: true,
     onError: async (ctx) => {
       console.dir(ctx);
