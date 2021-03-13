d146c7db0f3a65c6d4161ef52b584c01e20e6567
Georgy Farniev <gfarniev@icloud.com>
2021-03-13 12:07:11 +0800

    add test infrastructure

---
+++ tests/refs.spec.ts
@@ -0,0 +1,12 @@
+import 'jest'
+import { Document, Schema } from 'mongoose'
+
+async function collectErrors(schema: Schema, doc: Document) {}
+
+describe('Refs validation', () => {
+  test('basic ref errors validation', () => {
+    expect(true).toBeTruthy()
+  })
+
+})
+
